c7b0bf8c6a0a1e873eeca884301dfe32533878be
jyothiprakashk <jyothi.prakash@in-d.ai>
2021-03-05 11:46:36 +0530

    initial

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react'
+// import React from 'react'
 
-import { ExampleComponent } from '@jyothi/pg-bar-react'
+// import { ExampleComponent } from '@jyothi/pg-bar-react'
+// import '@jyothi/pg-bar-react/dist/index.css'
+
+// const App = () => {
+//   return <ExampleComponent text="Create React Library Example 😄" />
+// }
+
+// export default App
+import React, { useState, useEffect } from 'react'
+
+import ProgressBar from '@jyothi/pg-bar-react'
 import '@jyothi/pg-bar-react/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [completed, setCompleted] = useState(0)
+
+  useEffect(() => {
+    setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000)
+  }, [])
+
+  return (
+    <div className='App'>
+      <ProgressBar bgcolor={'#6a1b9a'} completed={completed} />
+    </div>
+  )
 }
 
 export default App
